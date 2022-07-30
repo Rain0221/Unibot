e9f299d71377c83a38949ff1b59d20b6d3dbd364
綿菓子ウニ <unijzlsx@gmail.com>
2022-07-31 02:30:03 +0800

    Update config.example.py

--- modules/config.example.py
+++ modules/config.example.py
@@ -9,19 +9,19 @@ proxies = {
 }
 # 不用代理改None
 ispredict = True
-bearer_token = ''  # 需注册推特开发者
-piccacheurl = ''  # 频道bot发送图片url前缀（带/）
-charturl = ''  # 频道bot发送谱面预览url前缀（带/）
-asseturl = 'https://assets.sekai.unijzlsx.com/'  # pjsk资源文件地址
-whitelist = []
+bearer_token = ''  # 需注册推特开发者（推车用）
+piccacheurl = ''  # 频道bot发送图片url前缀（带/）（频道bot用）
+charturl = ''  # 频道bot发送谱面预览url前缀（带/）（频道bot用）
+asseturl = 'https://assets.sekai.unijzlsx.com/'  # pjsk资源文件地址（频道bot用）
+whitelist = []  # 群bot猜曲白名单
 wordcloud = []  # 词云开启群
 loghtml = ''  # 昵称设置日志保存目录
-rsshub = ''  # rsshub地址
-twitterlist = ''  # 抓取的推特列表地址
-googleapiskey = ''  # 谷歌开发者key
+rsshub = ''  # rsshub地址（推特推送用 详见https://github.com/watagashi-uni/twitterpush）
+twitterlist = ''  # 抓取的推特列表地址（推特推送用 详见https://github.com/watagashi-uni/twitterpush）
+googleapiskey = ''  # 谷歌开发者key（推特推送用 详见https://github.com/watagashi-uni/twitterpush）
 # 百度翻译APP ID
-appID = ''
+appID = ''  # 机翻推特命令用
 # 百度翻译密钥
-secretKey = ''
+secretKey = ''  # 机翻推特命令用
 block = []  # bot拉黑
 msggroup = 123  # 发送bot管理消息的群
