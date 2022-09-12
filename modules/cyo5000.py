a7d84b02d76ad89c832fad645d7ace3a666d5059
綿菓子ウニ <unijzlsx@gmail.com>
2022-09-12 23:00:28 +0800

    Update cyo5000.py

--- modules/cyo5000.py
+++ modules/cyo5000.py
@@ -1,16 +1,16 @@
 import requests
 
-from modules.config import proxies
+from modules.config import proxies, cyo5000url
 
 
 def cyo5000(para0, para1, filename, israinbow=False):
     if israinbow:
-        url = f'https://unibot-cyo5000.herokuapp.com/image?top={para0}&bottom={para1}&noalpha=true&rainbow=true'
+        url = f'{cyo5000url}image?top={para0}&bottom={para1}&noalpha=true&rainbow=true'
     else:
-        url = f'https://unibot-cyo5000.herokuapp.com/image?top={para0}&bottom={para1}&noalpha=true'
+        url = f'{cyo5000url}image?top={para0}&bottom={para1}&noalpha=true'
     resp = requests.get(url, proxies=proxies)
     if 'Bad Request' in resp.text:
-        url = f'https://unibot-cyo5000.herokuapp.com/image?top=Bad Request&bottom=Bad Request&noalpha=true'
+        url = f'{cyo5000url}image?top=Bad Request&bottom=Bad Request&noalpha=true'
         resp = requests.get(url, proxies=proxies)
     with open(filename, 'wb') as f:
         f.write(resp.content)
